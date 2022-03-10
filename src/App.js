c36916fe57a41925c54c8354d92bb0bcd370a6b4
Mahmoud Mostafa Mahmoud Attia <mahmoud309mostafa@gmail.com>
2022-03-10 11:37:36 +0200

    add State to FavList

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './Component/Footer/Footer';
-import Navbar from './Component/NavBar/NavBar';
-import Productpage from './Component/productPage/productPage';
-import Shop from './Pages/Shop/Shop';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Component/Footer/Footer";
+import Navbar from "./Component/NavBar/NavBar";
+import Productpage from "./Component/productPage/productPage";
+import Shop from "./Pages/Shop/Shop";
 import { Home } from "./Pages";
-import Cart from './Pages/Cart/Cart'
-import Blog from './Pages/Blog/Blog';
-import Blogdetailed from './Component/BlogDetailed/blogDetailed';
-import About from './Pages/About/About';
-
-// import FavoriteList from './Component/favoriteList/favoriteList';
-//pathes-allias 
-//lazy loading in react 
-//suspence in react 
+import Cart from "./Pages/Cart/Cart";
+import Blog from "./Pages/Blog/Blog";
+import Blogdetailed from "./Component/BlogDetailed/blogDetailed";
+import About from "./Pages/About/About";
+import { useState } from "react";
+//pathes-allias
+//lazy loading in react
+//suspence in react
 function App() {
-    return ( 
-        <>
-            <Navbar />
-            <Routes>
-                <Route path = '/' element = {<Home />}/> 
-                <Route path='/shop' element={<Shop />}/>
-                <Route path='/cart' element={<Cart />}/>
-                <Route path='/blog' element={<Blog/>}/>
-                <Route path='/about' element={<About/>}/>
-                <Route path='/detailed' element={<Blogdetailed/>}/>
-                <Route path= '/:id' element={<Productpage/>}/>
-                {/* <Route path='/favoriteList' element={<FavoriteList/>} /> */}
-            </Routes>
-            <Footer />
-        </> 
-    );
+  const [activeFav, setActiveFav] = useState(false);
+
+  return (
+    <>
+      <Navbar setActiveFav={setActiveFav} />
+      <Routes>
+        <Route path="/" element={<Home activeFav={activeFav} />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/detailed" element={<Blogdetailed />} />
+        <Route path="/:id" element={<Productpage />} />
+      </Routes>
+      {console.log(activeFav)}
+      <Footer />
+    </>
+  );
 }
 
 export default App;
