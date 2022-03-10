ea0f326f80cb6c65cee033aa6564f4ec66147cad
Mahmoud Mostafa Mahmoud Attia <mahmoud309mostafa@gmail.com>
2022-03-10 11:39:02 +0200

    set State to open ,close favList

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,18 @@
-import FavoriteList from '../../Component/favoriteList/favoriteList';
-import Featuares from '../../Component/Featuares/Featuares';
-import Productgrid from '../../Component/Products/Products';
-import Slider from '../../Component/Slider/Slider';
+import FavoriteList from "../../Component/favoriteList/favoriteList";
+import Featuares from "../../Component/Featuares/Featuares";
+import Productgrid from "../../Component/Products/Products";
+import Slider from "../../Component/Slider/Slider";
 
-const Home = () => {
-    return (
-        <>
-            <Slider/>
-            <FavoriteList/>
-            <Featuares />
-            <Productgrid/>
-        </>
-    );
-}
+const Home = ({ activeFav }) => {
+  return (
+    <>
+      <Slider />
+      <FavoriteList activeFav={activeFav} />
+      <Featuares />
+      <Productgrid />
+      {console.log(activeFav)}
+    </>
+  );
+};
 
 export default Home;
